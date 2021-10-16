4f2a6e3f0c5dcf8fdde9a329afad1ba8301b0f53
kkentes <kevin.fuentesk@gmail.com>
2021-10-16 11:08:39 -0600

    Reserve-plus

---
+++ src/core/config.js
@@ -0,0 +1,12 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+
+    apiKey: "AIzaSyAaHFrUYSv-guF3Nhn1gsdMYh4M9qa58w0",
+    authDomain: "reserve-plus-a5720.firebaseapp.com",
+    projectId: "reserve-plus-a5720",
+    storageBucket: "reserve-plus-a5720.appspot.com",
+    messagingSenderId: "144069185824",
+    appId: "1:144069185824:web:36720276a77eefac42c745",
+    measurementId: "G-44JXMR3WHK"
+
+};
